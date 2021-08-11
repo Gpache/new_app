31510ca12bf18c1042f72597f088afc0937b8e8d
Gpache <gastonp06@gmail.com>
2021-08-11 13:24:11 -0300

    web push

---
+++ components/Layout/ContentFooter.vue
@@ -0,0 +1,19 @@
+<template>
+  <footer class="footer">
+    <div class="container-fluid">
+      <div class="copyright">
+         Copyright &copy; {{ year }} - <a href="https://www.segurit.ga">SEGURIT</a>
+      </div>
+    </div>
+  </footer>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      year: new Date().getFullYear()
+    };
+  }
+};
+</script>
+<style></style>
